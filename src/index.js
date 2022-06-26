af44d5207bde3a33e7d018cb0e16b4e25bf80225
ArtemDzhulai <dzhulai.artem@gmail.com>
2022-06-26 17:24:02 +0300

    feat: complet task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+n = n + "";
+let result = n.split('').reverse().join('');
+if (result[result.length - 1] === '-'){
+    return result.slice(0, result.length -1);
+}else{
+return result;
+}
 }
